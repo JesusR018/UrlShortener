022756d7abc7612bf3ac478d3b4e4b0b7b1b1b4a
Jesus Ramirez <jesusmanuelr018@gmail.com>
2024-03-19 22:20:47 +0100

    Add database configuration

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using URL_Shortener.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
